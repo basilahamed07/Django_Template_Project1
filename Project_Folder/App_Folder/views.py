a9ac3b5c653ff9e658607e807f58ef34aa760951
basilahamed07 <basilahamed46@gmail.com>
2024-08-29 00:47:44 +0530

    TAsk Completed

--- Project_Folder/App_Folder/views.py
+++ Project_Folder/App_Folder/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import *
+from .forms import *
 from django.views.generic.edit import CreateView
 
 # Create your views here.
 def landing_page(request):
-    latest_data = Post.objects.all().order_by("-time")[0:3]
-    return render(request,"App_Folder/landingpage.html",{"data":latest_data})
+    latest_data = Post.objects.all().order_by("-date")[0:3]
+    if request.method == 'POST':
+        form=commandForms(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form=commandForms()
+    return render(request,"App_Folder/landingpage.html",{"data":latest_data,"form":form})
 
 def all_post(request):
     data = Post.objects.all()
@@ -13,7 +20,11 @@ def all_post(request):
 
 def dataretrive(request,uniqe):
     posts = Post.objects.get(slug=uniqe)
-    return render(request,"App_Folder/post_details.html",{"data":posts})
+
+    #to get the ony one value
+    owner_id = Post.objects.only('id').get(slug=uniqe).id
+    commands = command.objects.filter(post_id=owner_id)
+    return render(request,"App_Folder/post_details.html",{"data":posts,"command":commands})
 
 
 def thankyou(request):
@@ -29,3 +40,4 @@ class ImageCreateView(CreateView):
     template_name = "App_Folder/image_form.html"
     success_url = "thankyou"
     fields = "__all__"
+
